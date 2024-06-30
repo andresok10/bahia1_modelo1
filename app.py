3f24cf4eff01925317f46e17de468db4fa7431ef
dave <okxgarcia@gmail.com>
2024-06-30 17:23:30 -0500

    sss

--- app.py
+++ app.py
@@ -43,6 +43,8 @@ except:
     pass
 
 if __name__ == '__main__':
+    #app.app_context().push()
+    #db.create_all()
     csrf = CSRFProtect(app)
     app.run(host='0.0.0.0', debug=True)
     #app.run(debug=True)
